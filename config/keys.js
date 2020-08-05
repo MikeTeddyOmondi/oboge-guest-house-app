107bc518569bda54a7c86537b4e009d28c3783a0
MikeTeddyOmondi <mike_omondi@outlook.com>
2020-08-05 19:41:59 +0300

    Updated files

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,12 @@
 require('dotenv').config()
 
 if (process.env.NODE_ENV !== 'production') {
-    dbPassword = 'mongodb://localhost:27017/admin-db';
+    dbPassword = 'mongodb+srv://user_db:user_db123@guest-house-db.uqp8l.gcp.mongodb.net/guest-house-db?retryWrites=true&w=majority'
 } else {
-    dbPassword = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@guest-house-db.uqp8l.gcp.mongodb.net/' + process.env.CLUSTER_NAME + '?retryWrites=true&w=majority'
-}
+    dbPassword = 'mongodb://localhost:27017/admin-db';}
 
 //dbPassword = 'mongodb://localhost:27017/admin-db';
 //dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
-//dbPassword = 'mongodb+srv://DB_USER:DB_PASSWORD@guest-house-db.uqp8l.gcp.mongodb.net/CLUSTER_NAME?retryWrites=true&w=majority
 
 module.exports = {
     mongoURI: dbPassword
