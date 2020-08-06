2ab5404e8648b990c6e99bfd4f108fcfb46e7206
MikeTeddyOmondi <mike_omondi@outlook.com>
2020-08-06 15:10:02 +0300

    Updated files

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 if (process.env.NODE_ENV !== 'production') {
     dbPassword = 'mongodb+srv://user_db:user_db123@guest-house-db.uqp8l.gcp.mongodb.net/guest-house-db?retryWrites=true&w=majority'
 } else {
-    dbPassword = 'mongodb://localhost:27017/admin-db';}
+    dbPassword = 'mongodb://localhost:27017/admin-db';
+}
 
 //dbPassword = 'mongodb://localhost:27017/admin-db';
 //dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
